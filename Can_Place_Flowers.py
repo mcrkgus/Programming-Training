e57fcd5cd504b613cadf02ff43c7c1533c2c125c
mcrkgus <101050833+mcrkgus@users.noreply.github.com>
2022-11-03 15:44:39 +0900

    Create Can_Place_Flowers.py

---
+++ Can_Place_Flowers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        cnt = 0
+        if n == 0 : return True
+        
+        if len(flowerbed) == 1 :
+            if flowerbed[0] == 0 :
+                return True
+            return False
+        flowerbed = [0] + flowerbed + [0]
+        
+        for i in range(1, len(flowerbed)-1) :
+            if flowerbed[i] == flowerbed[i-1] == flowerbed[i+1] == 0 :
+                flowerbed[i] = 1
+                cnt += 1
+        
+        if cnt >= n :
+            return True
+        return False
