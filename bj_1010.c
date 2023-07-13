1d6b144db61138007932ce35b2b670320f2253f5
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-07-13 21:43:51 +0900

    Create bj_1010.c

---
+++ bj_1010.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+double fac(int m)
+{
+	if(m==0)
+		return 1;
+	return m * fac(m-1);
+}
+double C(int m, int n)
+{
+	return fac(m) / (fac(m-n)*fac(n));
+}
+int main()
+{
+	int cnt = 0;
+	int n_site=0;
+	int m_site=0;
+	scanf("%d", &cnt);
+
+	for(int i=0; i<cnt; i++) {
+		scanf("%d %d", &n_site, &m_site);
+		printf("%.lf\n", C(m_site, n_site));
+
+	}
+
+	return 0;
+}
