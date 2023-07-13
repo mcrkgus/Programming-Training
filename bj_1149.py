7f54981ec2b8ea7798ab6c65424e59d5f3b70553
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-07-13 21:46:52 +0900

    Create bj_1149.py

---
+++ bj_1149.py
@@ -0,0 +1,13 @@
+N = int(input())
+dp = []
+for i in range(N) :
+    cost = list(map(int, input().split()))
+    dp.append(cost)
+
+
+for i in range(1, N) :
+    dp[i][0] += min(dp[i-1][1], dp[i-1][2])
+    dp[i][1] += min(dp[i-1][0], dp[i-1][2])   
+    dp[i][2] += min(dp[i-1][0], dp[i-1][1])
+
+print(min(dp[-1]))
