1a348cbf073996b866995b5c31aaccee59d95e0d
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-07-14 00:06:10 +0900

    Create bj_1157.py

---
+++ bj_1157.py
@@ -0,0 +1,13 @@
+word = input().lower()
+word_list = list(set(word))
+cnt = []
+
+for i in word_list :
+    count = word.count(i)
+    cnt.append(count)
+    
+    
+if cnt.count(max(cnt)) >= 2 :
+    print("?")
+else :
+    print(word_list[(cnt.index(max(cnt)))].upper())
