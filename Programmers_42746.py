74334aed0390938eb6a20e4e4037c17a8ad2e2d6
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-08-29 20:39:33 +0900

    Progrmmers - 가장 큰 수

---
+++ Programmers_42746.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = ''
+
+    total_number = list(map(str, numbers))   
+    total_number.sort(key=lambda x: x*3, reverse=True)
+
+    for i in total_number : 
+        answer+=i
+
+    return str(int(answer))
