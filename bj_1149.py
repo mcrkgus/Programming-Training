5c0ca05cc089f45ab3ada927107c8398e9e5c1ff
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-09-17 22:59:40 +0900

    baekjoon 1149

--- bj_1149.py
+++ bj_1149.py
@@ -4,7 +4,6 @@ for i in range(N) :
     cost = list(map(int, input().split()))
     dp.append(cost)
 
-
 for i in range(1, N) :
     dp[i][0] += min(dp[i-1][1], dp[i-1][2])
     dp[i][1] += min(dp[i-1][0], dp[i-1][2])   
