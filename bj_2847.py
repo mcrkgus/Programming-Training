cbb4a70972f5dd55ecac21ff69551e5df45db5cc
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-10-12 14:15:23 +0900

    baekjoon 2847

---
+++ bj_2847.py
@@ -0,0 +1,11 @@
+N = int(input())
+score = []
+for i in range(N) :
+    score.append(int(input()))
+
+count = 0
+for i in range(N-2, -1, -1) :
+    if score[i] >= score[i+1] :
+        count += score[i] - score[i+1] + 1
+        score[i] = score[i+1] - 1
+print(count)
