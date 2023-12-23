323a659c1ba824eb4fe83740ed1110a3c291f328
Gahyun Kim <101050833+mcrkgus@users.noreply.github.com>
2023-12-23 16:51:36 +0900

    baekjoon 1205

--- bj_1205.py
+++ bj_1205.py
@@ -6,7 +6,6 @@ score_list = list(map(int, input().split()))
 score_list.sort(reverse=True)
 
 
-# 이미 랭킹 리스트가 꽉 차 있으면서 최저 점수와 점수가 같거나 작을 경우
 if (P == len(score_list)) :
     if (score_list[-1] <= score) :
         res = -1
